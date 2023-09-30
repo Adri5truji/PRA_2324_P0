0930fc55dd6987796289b7290f63bc31d34dc593
ATRUDED <atruded@alumno.upv.es@ldsic-vdi08.upvnet.upv.es>
2023-09-30 17:48:15 +0200

    Confirmo el.h

---
+++ brazo.h
@@ -0,0 +1,19 @@
+#include <iostream>
+
+class BrazoRobotico{
+	private:
+		double x;
+		double y;
+		double z;
+		bool objeto;
+	public:
+		BrazoRobotico(double x, double y, double z, bool objeto);
+		double getX();
+		double getY();
+		double getZ();
+		double getObjeto();
+		void coger();
+		void soltar();
+		void mover(double a, double b, double c);
+};
+		
